8efeb35887db1b878749754a1642cda3d5257295
Sandeep Suthar <ss.suthar.004@gmail.com>
2025-11-30 10:56:41 +0530

    commit

--- 02. React from Scratch - part 1/00. React/04. tailwindReact/src/App.jsx
+++ 02. React from Scratch - part 1/00. React/04. tailwindReact/src/App.jsx
@@ -2,14 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card.jsx'
 
 function App() {
 
   return (
     <>
-      <h1 className='bg-green-500 text-black p-6 rounded-xl'>Tailwind Test</h1>
+      <h1 className='bg-green-500  p-6 rounded-xl text-red-500 '>Tailwind Test</h1>
+      
+      <Card />
+      <Card src="https://www.tutorialkart.com/cdn/title_logo_100px.png" />
     </>
   )
 }
 
 export default App
+ 
