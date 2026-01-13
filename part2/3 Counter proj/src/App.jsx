7d29c8ca8e1715e51019ef381bb7c19ca849eb41
Sandeep Suthar <ss.suthar.004@gmail.com>
2026-01-13 10:30:07 +0530

    *

--- part2/3 Counter proj/src/App.jsx
+++ part2/3 Counter proj/src/App.jsx
@@ -1,30 +1,34 @@
 import "./App.css"
+import { useEffec, useState } from "react"
+
 
 export default function App(){
+  const [count, setCount] = useState(0)
+  const [toSet, setTOSet] = useState(0)
+
+
+  //// const incremetHander = (prev) => {
+  //   //// setCount(numValue + 1)
+  //  // setCount((prev) => prev + 1)
+  // }
   return (
     <>
-      <h1>Counter</h1>
-      <div className="card">Count is 54</div>
+      <h1>Counter is {count}</h1>
+      <div className="card">Count is {count}</div>
 
       <div>
-        <button
-        onClick={() => {}}
-        style={{margin: "0 5px"}}  
-        >
+        <button onClick={() => setCount((count) => count + 1) } style={{margin: "0 5px"}} >
           Increase
         </button>
         
-        <button
-        onClick={() => {}}
-        style={{margin: "0 5px"}} >
+        <button onClick={() => setCount((count) => (Math.max(count - 1, 0)))} style={{margin: "0 5px"}} >
           Decrease
         </button>
 
-        <button
-        onClick={() => {}}
-        style={{margin: "0 5px"}} >
+        <button onClick={() => setCount((count) => 0)} style={{margin: "0 5px"}} >
           Reset
         </button>
+
         <div style={{margin:"10px 0"}}>
           <input 
             style={{
@@ -32,17 +36,14 @@ export default function App(){
               border: "1px solid white",
               margin: "0 5px",
               padding: "0.6em 1.2em"
-            }}
-            value="8"
-            onChange={() => {}}
-            type="text" 
-            name="" 
-            id="" 
+            }} value={toSet} onChange={(e) => setTOSet(Number(e.target.value))} type="text"  name=""  id="" 
           />
-          <button
-            style={{margin: "0 5px"}}
-            onClick={() => {}}
-          >set to 8</button>
+          <button style={{margin: "0 5px"}} onClick={() => {
+            setCount(Number(toSet))
+            setTOSet(0)
+          }}>
+            set to {toSet}
+          </button>
         </div>
       </div>
     </>
