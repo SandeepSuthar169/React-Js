a6224e6d064b8a3330aadb2de6b2e3882495f676
Sandeep Suthar <ss.suthar.004@gmail.com>
2026-01-20 10:29:00 +0530

    zustand counter

---
+++ part2/7 Zustaned/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { useState } from 'react'
+import Counter from './components/Counter'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Zustand</h1>
+      <Counter/>
+    </>
+  )
+}
+
+export default App
